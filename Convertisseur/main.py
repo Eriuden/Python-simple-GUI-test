dcd809445ba5e4d513974de6f46534ef89b80817
Eriuden <antoinecarrette@hotmail.fr>
2023-11-23 10:02:57 +0100

    app 1

---
+++ Convertisseur/main.py
@@ -0,0 +1,45 @@
+import PySimpleGUI as sg 
+
+layout =  [  
+    [
+        sg.Input(key = "INPUT"),
+        sg.Spin(["km/miles", "kg/livres", "sec/min"], key="UNITE"),
+        sg.Button("Conversion", key= "CONVERSION")
+    ],
+    [sg.text("Output", key= "OUTPUT")]
+]
+window = sg.Window("Convertisseur", layout).read()
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WIN_CLOSED:
+        break 
+
+    if event == "CONVERSION":
+        input_value = values["INPUT"]
+        if input_value.isnumeric():
+            match values["UNITS"]:
+                case "km/miles":
+                    output = round(float(input_value)) * 0.6214,2
+                    output_string = f"{input_value} km font {output} miles"
+
+                case "kg/livres":
+                    output = round(float(input_value)) * 2.20462,2
+                    output_string = f"{input_value} kg font {output} livres"
+
+                case "sec/min":
+                    output = round(float(input_value)) / 60,2
+                    output_string = f"{input_value} secondes font {output} minutes"
+
+            window["OUTPUT"].update(output_string)
+        else:   
+            window["OUTPUT"].update("Veuillez entrer un nombre")
+
+    if event == "Bouton2":
+        print("autre chose")
+        
+    if event == "TEXT":
+        print("Pression du texte")
+
+window.close()
