8d510616bba205a7950cf0bc17f991835c1c9654
Eriuden <antoinecarrette@hotmail.fr>
2023-11-26 18:56:18 +0100

    app 3

--- Chrono/main3.py
+++ Chrono/main3.py
@@ -1,27 +1,58 @@
 import PySimpleGUI as sg 
 from time import time 
 
-sg.theme("black")
-layout=[
-    [sg.Push(),sg.Image("cross.png", pad = 0, enable_events= True, key= "CLOSE")],
-    [sg.VPush()],
-    [sg.Text("time", font = "Young 50", key ="TIME")],
-    [   
-        sg.Button("Start", button_color= ("#FFFFFF","#FF0000"), border_width = 0, key = "STARTSTOP"),
-        sg.Button("Lap", button_color=("#FFFFFF","#ff0000"), border_width = 0, key = "LAP")
-    ],
-    [sg.VPush()]
-]
+def create_window():
 
-window = sg.Window(
-    "Chrono", layout,
-    size = (300,300),
-    no_titlebar = True,
-    element_justification = "center")
+    sg.theme("black")
+    layout=[
+        [sg.Push(),sg.Image("cross.png", pad = 0, enable_events= True, key= "CLOSE")],
+        [sg.VPush()],
+        [sg.Text("time", font = "Young 50", key ="TIME")],
+        [   
+        sg.Button("Start", button_color= ("#FFFFFF","#FF0000"), border_width = 0, key = "STARTSTOP"),
+        sg.Button("Lap", button_color=("#FFFFFF","#FF0000"), border_width = 0, key = "LAP", visible = False)
+        ],
+        [sg.Column([[sg.Text("row1")]], key = "LAPS")],
+        [sg.VPush()]
+    ]
+    return sg.Window(
+        "Chrono", layout,
+        size = (300,300),
+        no_titlebar = True,
+        element_justification = "center")
+
+
+window = create_window()
+
+start_time = 0
+active = False
+lap_amount = 1
 
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout = 10)
     if event in (sg.WIN_CLOSED, "CLOSE"):
         break
+    if event == "STARTSTOP":
+        if active :
+            active = False
+            window["STARTSTOP"].update("Reset")
+            window["LAP"].update(visible = False)
+        else:
+            if start_time > 0:
+                window.close()
+                window.create_window()
+                start_time = 0
+            else :
+                start_time =time()
+                active = True
+                window["STARTSTOP"].update("Stop")
+                window["LAP"].update(visible = True)
 
+    if active:
+        elapsed_time = round(time() - start_time)
+        window["TIME"].update(elapsed_time)
+    
+    if event =="LAP":
+        window.extend_layout(window["LAPS"], [[sg.Text(lap_amount), sg.VSeparator(), sg.Text(elapsed_time)]])
+        lap_amount += 1
 window.close()
